8d3d24b00afbc3d9d84a5594e3ea480cec2840c1
George Carr <gwcgreen3@gmail.com>
2021-12-03 17:11:56 +0000

    Main

---
+++ AdventOfCode/Puzzles/Main.cs
@@ -0,0 +1,16 @@
+﻿using Advent_of_Code.SonarSweep;
+
+// Dive
+Console.WriteLine("Dive");
+
+
+// Part One
+Console.WriteLine("--- Part One ---");
+Console.ReadLine();
+SonarSweep.Run(1);
+Console.ReadLine();
+//Part Two
+Console.WriteLine("--- Part Two ---");
+Console.ReadLine();
+SonarSweep.Run(2);
+Console.ReadLine();
--- AdventOfCode/Puzzles/SonarSweep.cs
+++ AdventOfCode/Puzzles/SonarSweep.cs
@@ -1,20 +1,4 @@
-﻿using Advent_of_Code.SonarSweep;
-
-// Dive
-Console.WriteLine("Dive");
-
-
-// Part One
-Console.WriteLine("--- Part One ---");
-Console.ReadLine();
-SonarSweep.Run(1);
-Console.ReadLine();
-//Part Two
-Console.WriteLine("--- Part Two ---");
-Console.ReadLine();
-SonarSweep.Run(2);
-Console.ReadLine();
-
+﻿
 namespace Advent_of_Code.SonarSweep
 {
     static class SonarSweep
