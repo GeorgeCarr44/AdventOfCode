4b9b98daf10b25ab178e88ba33a3de7b5361ca6f
George Carr <gwcgreen3@gmail.com>
2021-12-03 17:33:53 +0000

    Dive

--- AdventOfCode/Puzzles/Dive.cs
+++ AdventOfCode/Puzzles/Dive.cs
@@ -1,1 +1,70 @@
-﻿Console.WriteLine("Dive");
+﻿namespace Advent_of_Code.Dive
+{
+    static class Dive
+    {
+        enum Direction
+        {
+            up,
+            down,
+            forward,
+            backward
+        }
+
+        class Command
+        {
+            public Direction Direction;
+            public int Distance;
+
+            public Command(string input)
+            {
+                string[] splitInput = input.Split(' ');
+                Direction = (Direction)Enum.Parse(typeof(Direction), splitInput[0]);
+                Distance = int.Parse(splitInput[1]);
+            }
+        }
+
+        public static void Run(int task)
+        {
+
+            string[] input = System.IO.File.ReadAllLines(@"C:\Users\gwcgr\Documents\Code\AdventOfCode\AdventOfCode\Inputs\Dive.txt");
+
+            int depth = 0;
+            int horizontalPosition = 0;
+            int aim = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                Command command = new Command(input[i]);
+
+                switch (command.Direction)
+                {
+                    case Direction.up:
+                        if (task == 1)
+                            depth -= command.Distance;
+                        else if (task == 2)
+                            aim -= command.Distance;
+                        break;
+                    
+                    case Direction.down:
+                        if (task == 1)
+                            depth += command.Distance;
+                        else if (task == 2)
+                            aim += command.Distance;
+                        break;
+
+                    case Direction.forward:
+                        if (task == 1)
+                            horizontalPosition += command.Distance;
+                        else if (task == 2)
+                            horizontalPosition += command.Distance;
+                            depth += aim * command.Distance;
+                        break;
+
+                    case Direction.backward:
+                        break;
+                }
+            }
+            Console.WriteLine(depth * horizontalPosition);
+        }
+    }
+}
--- AdventOfCode/Puzzles/Main.cs
+++ AdventOfCode/Puzzles/Main.cs
@@ -1,16 +1,33 @@
 ﻿using Advent_of_Code.SonarSweep;
+using Advent_of_Code.Dive;
 
-// Dive
+//// Sonar Sweep Day One
+//Console.WriteLine("Sonar Sweep");
+
+
+//// Part One
+//Console.WriteLine("--- Part One ---");
+//Console.ReadLine();
+//SonarSweep.Run(1);
+//Console.ReadLine();
+////Part Two
+//Console.WriteLine("--- Part Two ---");
+//Console.ReadLine();
+//SonarSweep.Run(2);
+//Console.ReadLine();
+
+
+//// Dive Day Two
 Console.WriteLine("Dive");
-
-
 // Part One
 Console.WriteLine("--- Part One ---");
 Console.ReadLine();
-SonarSweep.Run(1);
+Dive.Run(1);
 Console.ReadLine();
 //Part Two
 Console.WriteLine("--- Part Two ---");
 Console.ReadLine();
-SonarSweep.Run(2);
+Dive.Run(2);
 Console.ReadLine();
+
+
