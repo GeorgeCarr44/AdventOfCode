6f77371241351db239e8d339a5a53dc6582cb7cd
George Carr <gwcgreen3@gmail.com>
2022-12-05 20:10:18 +0000

    Part 1

--- AdventOfCode/Puzzles/CampCleanup.cs
+++ AdventOfCode/Puzzles/CampCleanup.cs
@@ -38,18 +38,8 @@ namespace AdventOfCode.Puzzles
             foreach (var line in lines)
             {
                 var pair = line.Split(",");
-                Input.Add(new string[] { GetAssignmentArray(pair[0]), GetAssignmentArray(pair[1]) });
+                Input.Add(new string[] { pair[0], pair[1] });
             }
-        }
-
-        private static string GetAssignmentArray(string assignments)
-        {
-            string[] startend = assignments.Split('-');
-
-            int startInt = Int32.Parse(startend[0]);
-            int endInt = Int32.Parse(startend[1]);
-
-            return string.Join(",", Enumerable.Range(startInt,  endInt - startInt + 1));
         }
 
         private static void Part1()
@@ -58,9 +48,17 @@ namespace AdventOfCode.Puzzles
 
             foreach (var pair in Input)
             {
-                Array.Sort(pair, (x, y) => x.Length.CompareTo(y.Length));
-                if(pair[1].Contains(pair[0]))
-                    fullContain++;
+                int[]? first = pair[0].Split("-")?.Select(Int32.Parse)?.ToArray();
+                int[]? second = pair[1].Split("-")?.Select(Int32.Parse)?.ToArray();
+
+                if(first != null && second != null)
+                {
+                    if (first[0] >= second[0] && first[1] <= second[1] || second[0] >= first[0] && second[1] <= first[1])
+                    {
+                        fullContain++;
+                    }
+                
+                }
             }
 
             Console.WriteLine("Full Contain");
