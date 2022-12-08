a51b1841db9fbecb43e381e5de34f3e99d3dad95
George Carr <gwcgreen3@gmail.com>
2022-12-08 19:56:19 +0000

    Part 1 and 2

--- AdventOfCode/Puzzles/Trees.cs
+++ AdventOfCode/Puzzles/Trees.cs
@@ -11,134 +11,81 @@ namespace AdventOfCode.Puzzles
     {
         public static string[] Input { get; set; }
 
-        public static Tree[,]? Forest { get; set; }
-
-        public class Tree
-        {
-            public int Height { get; set; }
-            public bool? Visible { get; set; }
-            readonly int XPos;
-            readonly int YPos;
-            public Tree[] Neigbours { get; set; }
-
-            public Tree(int x, int y, int height)
-            {
-                Height = height;
-                Visible = null;
-                XPos = x;
-                YPos = y;
-            }
-
-            public void LookAtNeigbours()
-            {
-                //Out of Forest Exception
-                Neigbours = new Tree[4];
-
-                if (XPos - 1 >= 0)
-                    Neigbours[0] = Forest[XPos - 1, YPos];
-                if (YPos + 1 <= Forest.GetLength(0) - 1)
-                    Neigbours[1] = Forest[XPos, YPos + 1];
-                if (XPos + 1 <= Forest.GetLength(1) - 1)
-                    Neigbours[2] = Forest[XPos + 1, YPos];
-                if (YPos - 1 >= 0)
-                    Neigbours[3] = Forest[XPos, YPos - 1];
-            }
-
-            public void CheckVisible()
-            {
-                if(Neigbours.Any(x => x == null))
-                    Visible = true;
-                else if(Neigbours.Any(x => x.Visible == true && x.Height < this.Height))
-                    Visible = true;
-                else if(Neigbours.All(x => x.Visible != null))
-                    Visible = false;
-            }
-        }
-
         public static void Run()
         {
             Input = File.ReadAllLines(@"Inputs\Trees.txt");
-            Part1();
-            Part2();
-        }
 
-        private static void Part1()
-        {
-            GenerateForest();
-            DrawForest();
-        }
-
-        private static void Part2()
-        {
-
-        }
-
-        private static void GenerateForest()
-        {
-
-            int X = Input[0].Length;
-            int Y = Input.Length;
-
-            Forest = new Tree[X, Y];
-
-            for (int x = 0; x < X; x++)
+            int visibleTrees = 0;
+            int scenicScore = 0;
+            for (int x = 0; x < Input.Length; x++)
             {
-                int[] currentLineCharArray = Input[x].Select(x => Int32.Parse(x.ToString())).ToArray();
-                for (int y = 0; y < Y; y++)
+                for (int y = 0; y < Input[x].Length; y++)
                 {
-                    Forest[x, y] = new Tree(x, y, Convert.ToInt32(currentLineCharArray[y]));
+                    if (CheckVisible(x, y))
+                        visibleTrees++;
+
+                    int currentScore = CheckScenicScore(x, y);
+                    if (currentScore > scenicScore)
+                        scenicScore = currentScore;
                 }
             }
-
-            foreach (var tree in Forest)
-            {
-                tree.LookAtNeigbours();
-            }
-
-            for (int i = 0; i < 100; i++)
-            {
-                foreach (var tree in Forest)
-                { 
-                    tree.CheckVisible();
-                }
-
-            }
-
-            int count = 0;
-
-            foreach (var tree in Forest)
-            {
-                if(tree.Visible == true)
-                    count++;
-            }
-            Console.WriteLine(count);
+            Console.WriteLine("--- Part 1 ---");
+            Console.WriteLine(visibleTrees);
+            Console.WriteLine("--- Part 2 ---");
+            Console.WriteLine(scenicScore);
         }
 
-
-        private static void DrawForest()
+        private static bool CheckVisible(int x, int y)
         {
+            //returns first true
+            var startValue = Input[x][y];
 
-            int X = Input[0].Length;
-            int Y = Input.Length;
-
-            for (int x = 0; x < X; x++)
-            {
-                string line = "";
-                var currentLineCharArray = Input[x].ToCharArray();
-                for (int y = 0; y < Y; y++)
-                {
-                    line += Forest[x, y].Height;
-                    if(Forest[x, y].Visible == true)
-                        line += ' ';
-                    else
-                        line +=  '!';
-                    line += ' ';
-                }
-                Console.WriteLine(line);
-            }
+            return IsVisible(x, y, -1, 0, startValue) || IsVisible(x, y, 0, 1, startValue) || IsVisible(x, y, 1, 0, startValue) || IsVisible(x, y, 0, -1, startValue);
         }
 
+        private static bool IsVisible(int x, int y, int xChange, int yChange, char startValue)
+        {
+            //Edge Pieces
+            if (x == 0 || y == 0 || x == Input[x].Length - 1 || y == Input.Length - 1)
+            {
+                return true;
+            }
 
+            //Cant be seen from the north
+            //Cant be out of range as returns when edge piece
+            if (startValue <= Input[x + xChange][y + yChange])
+            {
+                return false;
+            }
 
+            return IsVisible(x + xChange, y + yChange, xChange, yChange, startValue);
+        }
+
+        private static int CheckScenicScore(int x, int y)
+        {
+            //returns first true
+            var startValue = Input[x][y];
+
+            //If any tree is right on the edge the score will be 0 its multiplied
+            return ScenicScore(x, y, -1, 0, startValue) * ScenicScore(x, y, 0, 1, startValue) * ScenicScore(x, y, 1, 0, startValue) * ScenicScore(x, y, 0, -1, startValue);
+        }
+
+        private static int ScenicScore(int x, int y, int xChange, int yChange, char startValue)
+        {
+            //If its looking out of bounds
+            if (x + xChange == -1 || y + yChange == -1 || x + xChange == Input[x].Length || y + yChange == Input.Length)
+            {
+                return 0;
+            }
+
+            //Cant be seen from the north
+            //Cant be out of range as returns when edge piece
+            if (startValue <= Input[x + xChange][y + yChange])
+            {
+                return 1;
+            }
+
+            return 1 + ScenicScore(x + xChange, y + yChange, xChange, yChange, startValue);
+        }
     }
 }
