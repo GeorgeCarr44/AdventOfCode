9ffc97239880dd120bb8b9ec5681eceac93e3cdb
George Carr <george.carr@eploy.co.uk>
2024-12-10 13:05:38 +0000

    day 3 part 2 quick attempt

--- AdventOfCode/Puzzles/2024/MullItOver.cs
+++ AdventOfCode/Puzzles/2024/MullItOver.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,13 @@ namespace AdventOfCode.Puzzles._2024
     static class MullItOver
     {
         private static string[] Rows { get; set; }
+        private static string RunningCommand { get; set; }
+        private static string CaptureGroup { get; set; }
 
         public static void Run()
         {
             ReadInput(@"Inputs\2024\MullItOver.txt");
-            Part1();
+            Part2();
         }
 
         private static void Part1()
@@ -54,7 +57,75 @@ namespace AdventOfCode.Puzzles._2024
 
         private static void Part2()
         {
-            
+            bool mulEnabled = true;
+            bool captureNumber = false;
+            int? firstNumber = null;
+            int? secondNumber = null;
+
+
+            foreach (var row in Rows)
+            { 
+                RunningCommand = string.Empty;
+                CaptureGroup = string.Empty;
+                foreach (var currentChar in row.ToCharArray())
+                {
+                    if (captureNumber)
+                    {
+
+                        if (Char.IsNumber(currentChar))
+                        {
+                            CaptureGroup += currentChar;
+                        }
+                        else if (currentChar == ',')
+                        {
+                            if (RunningCommand == "mul_First_Param")
+
+
+
+                            if (firstNumber.HasValue)
+                            {
+                                firstNumber += currentChar;
+                            }
+                        }
+                        else
+                        {
+                            // Invalid format fail out.
+                            RunningCommand = string.Empty;
+                            CaptureGroup = string.Empty;
+                        }
+                    }
+                    else
+                    {
+                        if (CheckCommand(currentChar, "mul(") && mulEnabled)
+                        {
+                            // Look at next should be (
+                            captureNumber = true;
+                            // look at next 
+                        }
+
+                        if (CheckCommand(currentChar, "do()"))
+                            mulEnabled = true;
+                        if (CheckCommand(currentChar, "don't()"))
+                            mulEnabled = false;
+                    }
+                }
+                
+            }
+        }
+
+        private static bool CheckCommand(char currentChar, string command)
+        {
+            if (currentChar == command.ToCharArray()[RunningCommand.Length])
+            {
+                RunningCommand += currentChar;
+            }
+
+            if (RunningCommand == command)
+            {
+                RunningCommand = string.Empty;
+                return true;
+            }
+            return false;
         }
 
         private static void ReadInput(string input)
